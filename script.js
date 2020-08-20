a6e7ccf1be86c0a4799b7625b218f51fd269c5a1
Stephen <juriz_stephen@hotmail.co.uk>
2020-08-20 15:47:34 +0100

    Pull

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+setTimeout(function()
+{
+    document.getElementById("CV").innerHTML = '<a href="media/CV.pdf" download="CV.pdf">Download CV</a>';
+}
+,17500);
+
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
